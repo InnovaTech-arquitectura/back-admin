1cb93831022ea0a87dbe2904636638c6307babee
Sophia Aristizabal <saristizabal1017@gmail.com>
2024-09-22 01:54:02 -0500

    i finished all my unitary tests

--- demo/src/main/java/com/innovatech/demo/DTO/CourseDTONoID.java
+++ demo/src/main/java/com/innovatech/demo/DTO/CourseDTONoID.java
@@ -14,6 +14,7 @@ import java.sql.Date;
 
 @Getter
 @Data
+@NoArgsConstructor
 public class CourseDTONoID {
 
     String link;
--- demo/src/main/java/com/innovatech/demo/init/DBInitializer.java
+++ demo/src/main/java/com/innovatech/demo/init/DBInitializer.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
