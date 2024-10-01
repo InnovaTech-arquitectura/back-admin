012493e0ffa0c8b01b17d11a63725d6ede095907
Andrés García Montoya <61592522+andres345gm@users.noreply.github.com>
2024-09-30 22:13:26 -0500

    Changes in update

--- demo/src/main/java/com/innovatech/demo/Controller/LoginController.java
+++ demo/src/main/java/com/innovatech/demo/Controller/LoginController.java
@@ -8,8 +8,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +18,6 @@ import com.innovatech.demo.Service.UserService;
 
 @RestController
 @RequestMapping("/login")
-@CrossOrigin(origins = "http://localhost:4200")
 public class LoginController {
     @Autowired
     private UserService userService;
--- demo/src/main/java/com/innovatech/demo/Controller/ProfileController.java
+++ demo/src/main/java/com/innovatech/demo/Controller/ProfileController.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,7 +29,6 @@ import com.innovatech.demo.Service.UserService;
 
 @RestController
 @RequestMapping("/profile")
-@CrossOrigin(origins = "http://localhost:4200")
 public class ProfileController {
     @Autowired
     private UserService userService;
@@ -159,7 +157,7 @@ public class ProfileController {
                 userToUpdate.setRole(existingRole);
             }
 
-            userToUpdate = userService.save(userToUpdate);
+            userToUpdate = userService.update(userToUpdate);
             existingAdmin.setUser(userToUpdate);
 
             administrativeEmployeeService.save(existingAdmin);
