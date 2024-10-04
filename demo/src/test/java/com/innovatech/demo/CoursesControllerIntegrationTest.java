03f57537dbca6e79a488e30637fcae7879987214
Sophia Aristizabal <saristizabal1017@gmail.com>
2024-10-03 22:13:59 -0500

    cambios basicos pero no me funciona nada

--- demo/src/test/java/com/innovatech/demo/CoursesControllerIntegrationTest.java
+++ demo/src/test/java/com/innovatech/demo/CoursesControllerIntegrationTest.java
@@ -23,6 +23,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -42,6 +43,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
+@DirtiesContext
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 public class CoursesControllerIntegrationTest {
