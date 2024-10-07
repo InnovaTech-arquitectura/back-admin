42ece33ccceabc0652377aebbd77fc460fe7bfc7
Sophia Aristizabal <saristizabal1017@gmail.com>
2024-10-06 20:46:02 -0500

    changed the form in which i add the administrative admin

--- demo/src/main/java/com/innovatech/demo/Controller/PublicationController.java
+++ demo/src/main/java/com/innovatech/demo/Controller/PublicationController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +27,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.innovatech.demo.DTO.BannerDTO;
 import com.innovatech.demo.DTO.BannerInfoDTO;
 import com.innovatech.demo.Entity.Banner;
+import com.innovatech.demo.Entity.UserEntity;
+import com.innovatech.demo.Security.JWTGenerator;
 import com.innovatech.demo.Service.MinioService;
 import com.innovatech.demo.Service.PublicationService;
 
@@ -35,6 +39,7 @@ import io.minio.errors.InvalidResponseException;
 import io.minio.errors.ServerException;
 import io.minio.errors.XmlParserException;
 import org.apache.commons.io.IOUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @RestController
 @RequestMapping("/banner")
@@ -46,6 +51,9 @@ public class PublicationController {
 
     @Autowired
     private MinioService minioService;
+
+    @Autowired
+    private JWTGenerator jwtGenerator;
 
 
     @GetMapping("/all")
@@ -77,9 +85,14 @@ public class PublicationController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> editBanner(@PathVariable Long id, @ModelAttribute BannerDTO editedBannerDto) {
+    public ResponseEntity<?> editBanner(@PathVariable Long id, @ModelAttribute BannerDTO editedBannerDto,@RequestHeader("Authorization") String token) {
         try {
-            Banner editedBanner = publicationService.editBanner(id, editedBannerDto);
+
+            token = token.substring(7);
+
+            String email = jwtGenerator.getUserFromJwt(token);
+
+            Banner editedBanner = publicationService.editBanner(id, editedBannerDto, email);
 
             //save image in minio
             try {
@@ -99,10 +112,14 @@ public class PublicationController {
     }
 
     @PostMapping("/new")
-    public ResponseEntity<?> createBanner(@ModelAttribute BannerDTO newBannerDto) throws InvalidKeyException, ServerException, InsufficientDataException, ErrorResponseException, NoSuchAlgorithmException, InvalidResponseException, XmlParserException, InternalException, IOException {
+    public ResponseEntity<?> createBanner(@ModelAttribute BannerDTO newBannerDto, @RequestHeader("Authorization") String token) throws InvalidKeyException, ServerException, InsufficientDataException, ErrorResponseException, NoSuchAlgorithmException, InvalidResponseException, XmlParserException, InternalException, IOException {
         
         try {
-            Banner newBanner = publicationService.createBanner(newBannerDto);
+            token = token.substring(7);
+            //System.out.println("UNICORNIO "+jwtGenerator.getUserFromJwt(token));
+            String email = jwtGenerator.getUserFromJwt(token);
+            
+            Banner newBanner = publicationService.createBanner(newBannerDto,email);
 
             //save image in minio
             try {
